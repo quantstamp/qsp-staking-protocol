b293e698c4c2edc1e4b2be4adf611b7a1240d4e8
banescusebi <sebi@quantstamp.com>
2018-10-01 22:33:00 +0200

    Added primitive implementation of submitContract and primitive test

--- contracts/QSPb.sol
+++ contracts/QSPb.sol
@@ -9,7 +9,32 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract QSPb is Ownable {
   using LinkedListLib for LinkedListLib.LinkedList;
-  
+  using SafeMath for uint256;
+
+  enum PoolState { Initialized, // Pool was initialized, Insufficient stakes
+	           NotViolatedSstkIdep, // Policy Not Violated, Sufficient stakes, Insufficient deposit
+		   ViolatedSstkIdep, // Policy Violated, Sufficient stakes, Insufficient deposit
+		   NotViolatedSstkSdep, // Policy Not Violated, Sufficient stakes, Sufficient deposit
+		   ViolatedSstkSdep, // Policy Violated, Sufficient stakes, Sufficient deposit
+		   TimedOut, // Pool has timed-out due to insufficient stakes
+		   CanceledExpired } // Pool was canceled or policy has expired
+
+  struct Pool {
+    address c;
+    address owner;
+    uint maxPayout;
+    uint minStake;
+    uint deposit;
+    uint bonusExpert;
+    uint bonusFirstExpert;
+    uint payPeriod;
+    uint minSktTime;
+    uint timeout;
+    //mapping (address => uint) stakes;
+    //LinkedListLib.LinkedList stakingOrder;
+    uint timeOfInit;
+  }
+ 
   // constants used by LinkedListLib
   uint256 constant internal NULL = 0;
   uint256 constant internal HEAD = 0;
@@ -17,19 +42,22 @@ contract QSPb is Ownable {
   bool constant internal NEXT = true;
 		
   uint public balance;  
-	
-  struct Pool {
-    address owner;
-    uint stakeholderDeposit;
-    mapping (address => uint) stakes;
-    LinkedListLib.LinkedList stakingOrder;
-    uint timeOfInit;
+  Pool[] public pools;  
+
+  constructor() public {
+    balance = 0;
   }
 
-  function createPool(address c, uint maxPayout, uint minStake, uint iniDeposit, 
+  function submitContract(address c, uint maxPayout, uint minStake, 
                       uint bonusExpert, uint bonusFirstExp, uint payPeriod, 
-                      uint minStkTime, uint minStkPeriod, uint timeout) public {
+                      uint minStkTime, uint timeout) public payable {
 
+    balance.add(msg.value);
+    //LinkedListLib.LinkedList internal ll;
+
+    Pool memory p = Pool(c, msg.sender, maxPayout, minStake, msg.value, bonusExpert, 
+		  bonusFirstExp, payPeriod, minStkTime, timeout, now); 
+
+    pools.push(p);
   }
-
 }
--- test/QSPbTest.js
+++ test/QSPbTest.js
@@ -6,5 +6,18 @@ contract('QSPb', function(accounts){
   const staker = accounts[2];
   const candidateContract = accounts[3];
 
-  
-})
+  beforeEach(async function () {
+    qspb = await QSPb.deployed();
+    quantstamp_token = await QuantstampToken.deployed();
+    
+    // enable transfers before any payments are allowed
+    await quantstamp_token.enableTransfer({from : owner});
+    // transfer 100,000 QSP tokens to the requestor
+    await quantstamp_token.transfer(requestor, requestorBudget, {from : owner});
+  });
+
+  if ("submits contract", async function() {
+    qspb.submitContract(candidateContract, 10, 1, 3, 5, 15, 10000, 100);
+    assert(qspb.pools.length > 0);
+  }); 
+});
