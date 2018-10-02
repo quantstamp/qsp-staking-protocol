275d3ed487ebde55cbb9e449be4ca4cebcb92a51
banescusebi <sebi@quantstamp.com>
2018-10-03 00:23:31 +0200

    Addressed review comments from Alex

--- migrations/2_deploy_staking_contract.js
+++ migrations/2_deploy_staking_contract.js
@@ -1,8 +1,8 @@
 const QSPb = artifacts.require('QSPb');
 const LinkedListLib = artifacts.require('LinkedListLib');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(LinkedListLib)
-    .then(() => deployer.link(LinkedListLib, QSPb));
-  deployer.deploy(QSPb);
+    .then(() => deployer.link(LinkedListLib, QSPb))
+    .then(() => deployer.deploy(QSPb));
 };
--- truffle.js
+++ truffle.js
@@ -23,9 +23,6 @@ module.exports = {
       runs: 200
     }
   },
-  deploy: {
-    QSPb: false,
-  },
   networks: {
     development: {
       host: "127.0.0.1",
@@ -39,24 +36,6 @@ module.exports = {
       },
       network_id: 3,
       gas: 4007806,
-      gasPrice: 110000000000,
-      account: '0x0283c049ed4705e2d98c807dbafdaf725f34b8d2'
-    },
-    staging: {
-      provider:  function() {
-        return new HDWalletProvider(credentials.mnemonic, `https://ropsten.infura.io/${credentials.infura_apikey}`);
-      },
-      network_id: 3,
-      gas: 4007806,
-      gasPrice: 110000000000,
-      account: '0x0283c049ed4705e2d98c807dbafdaf725f34b8d2'
-    },
-    'dev-consensys': {
-      provider:  function() {
-        return new HDWalletProvider(credentials.mnemonic, `https://ropsten.infura.io/${credentials.infura_apikey}`);
-      },
-      network_id: 3,
-      gas: 4712388,
       gasPrice: 110000000000,
       account: '0x0283c049ed4705e2d98c807dbafdaf725f34b8d2'
     },
