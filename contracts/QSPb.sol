df74b4f95644682091b84526a8b88f5170399a7c
banescusebi <sebi@quantstamp.com>
2018-10-03 22:57:45 +0200

    Added Pool structure and createPool primitive implementation

--- contracts/QSPb.sol
+++ contracts/QSPb.sol
@@ -12,6 +12,22 @@ contract QSPb is Ownable {
     using LinkedListLib for LinkedListLib.LinkedList;
     using SafeMath for uint256;
  
+    struct Pool {
+        address c;
+        address owner;
+        uint maxPayout;
+        uint minStake;
+        uint deposit;
+        uint bonusExpert;
+        uint bonusFirstExpert;
+        uint payPeriod;
+        uint minSktTime;
+        uint timeout;
+        mapping (address => uint) stakes;
+        LinkedListLib.LinkedList stakingOrder;
+        uint timeOfInit;
+    }
+
     // constants used by LinkedListLib
     uint256 constant internal NULL = 0;
     uint256 constant internal HEAD = 0;
@@ -19,8 +35,26 @@ contract QSPb is Ownable {
     bool constant internal NEXT = true;
 
     uint public balance;  
+    Pool[] public pools;
   
     constructor() public {
         balance = 0;
+    }
+
+    function getPoolsLength() public view returns (uint) {
+        return pools.length;
+    }
+
+    function createPool(address c, uint maxPayout, uint minStake,
+                      uint bonusExpert, uint bonusFirstExp, uint payPeriod,
+                      uint minStkTime, uint timeout) public payable {
+
+        balance.add(msg.value);
+        mapping (address => uint) public stakes;
+	LinkedListLib.LinkedList public stakingOrder;
+
+        Pool memory p = Pool(c, msg.sender, maxPayout, minStake, msg.value, bonusExpert,
+			     bonusFirstExp, payPeriod, minStkTime, timeout, stakes, staking Order, now);
+        pools.push(p);
     }
 }
