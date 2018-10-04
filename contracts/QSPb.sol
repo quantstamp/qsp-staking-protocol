c3da260d052c9bb297be2bfa821528a38a8ae740
banescusebi <sebi@quantstamp.com>
2018-10-04 23:10:21 +0200

    Changed Pool structure and added Stake structure and stakes map separately

--- contracts/QSPb.sol
+++ contracts/QSPb.sol
@@ -11,9 +11,15 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract QSPb is Ownable {
     using LinkedListLib for LinkedListLib.LinkedList;
     using SafeMath for uint256;
- 
+
+    struct Stake {
+      address staker;
+      uint amount;
+    }
+
     struct Pool {
         address c;
+	address contractPolicy;
         address owner;
         uint maxPayout;
         uint minStake;
@@ -23,10 +29,11 @@ contract QSPb is Ownable {
         uint payPeriod;
         uint minSktTime;
         uint timeout;
-        mapping (address => uint) stakes;
-        LinkedListLib.LinkedList stakingOrder;
         uint timeOfInit;
     }
+
+    // store sha3(Pool) as the key of the mapping
+    mapping (bytes32 => Stake[]) stakes; 
 
     // constants used by LinkedListLib
     uint256 constant internal NULL = 0;
@@ -45,16 +52,16 @@ contract QSPb is Ownable {
         return pools.length;
     }
 
-    function createPool(address c, uint maxPayout, uint minStake,
+    function createPool(address c, address contractPolicy, uint maxPayout, uint minStake,
                       uint bonusExpert, uint bonusFirstExp, uint payPeriod,
                       uint minStkTime, uint timeout) public payable {
 
-        balance.add(msg.value);
-        mapping (address => uint) public stakes;
-	LinkedListLib.LinkedList public stakingOrder;
-
-        Pool memory p = Pool(c, msg.sender, maxPayout, minStake, msg.value, bonusExpert,
-			     bonusFirstExp, payPeriod, minStkTime, timeout, stakes, staking Order, now);
+        Pool memory p = Pool(c, contractPolicy, msg.sender, maxPayout, minStake, msg.value, 
+			     bonusExpert, bonusFirstExp, payPeriod, minStkTime, timeout, now);
         pools.push(p);
+    }
+
+    function stakeFunds(bytes32 poolHash) public payable {
+        stakes[poolHash].push(Stake(msg.sender, msg.value)); 
     }
 }
--- test/QSPbTest.js
+++ test/QSPbTest.js
@@ -1,12 +1,20 @@
 const QSPb = artifacts.require('QSPb');
 
 contract('QSPb', function(accounts){
-  
-  beforeEach(async function () {
-    qspb = await QSPb.deployed();
-  });
+    const candidateContract = accounts[0];
+    const contractPolicy = accounts[1];
 
-  it("should never fail", async function() {
-    assert.true;
-  }); 
+    beforeEach(async function () {
+        qspb = await QSPb.deployed();
+    });
+
+    it("should never fail", async function() {
+        assert.true;
+    });
+
+    it("should add a pool", async function() {
+        assert.equal(await qspb.balance.call(), 0);
+        await qspb.createPool(candidateContract, contractPolicy, 10, 1, 3, 5, 15, 10000, 100);
+        assert.equal(await qspb.getPoolsLength.call(), 1);
+    }); 
 });
