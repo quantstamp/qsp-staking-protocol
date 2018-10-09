9acfcccbeec40b570b9c99107a22c40a12717c88
banescusebi <sebi@quantstamp.com>
2018-10-09 22:50:22 +0200

    Addressed comments from Jan and Vajih

--- contracts/QSPb.sol
+++ contracts/QSPb.sol
@@ -3,13 +3,11 @@ pragma solidity 0.4.24;
 /// @title QSPb - is the smart contract representing the core of the Staking Protocol
 /// @author Sebastian Banescu
 
-import "./LinkedListLib.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract QSPb is Ownable {
-    using LinkedListLib for LinkedListLib.LinkedList;
     using SafeMath for uint256;
 
     struct Stake {
@@ -27,21 +25,14 @@ contract QSPb is Ownable {
         uint bonusExpert;
         uint bonusFirstExpert;
         uint payPeriod;
-        uint minStakeTime;
-        uint timeout;
-        uint timeOfInit;
+        uint minStakeTimeInBlocks; // the minimum number of blocks that funds need to be staked for 
+        uint timeoutInBlocks; // the number of blocks after which a pool is canceled if there are not enough stakes
+        uint timeOfInitInBlocks; // the block number when the pool was initialized
         string urlOfAuditReport;
     }
 
     // store sha3(Pool) as the key of the mapping
     mapping (bytes32 => Stake[]) public stakes; 
-
-    // constants used by LinkedListLib
-    uint256 constant internal NULL = 0;
-    uint256 constant internal HEAD = 0;
-    bool constant internal PREV = false;
-    bool constant internal NEXT = true;
-
     uint public balance;  
     Pool[] internal pools;
   
@@ -53,55 +44,55 @@ contract QSPb is Ownable {
         return pools.length;
     }
 
-    function getPoolCandidateContract(uint index) public constant returns(address) {
+    function getPoolCandidateContract(uint index) public view returns(address) {
         return pools[index].candidateContract;
     }
 
-    function getPoolContractPolicy(uint index) public constant returns(address) {
+    function getPoolContractPolicy(uint index) public view returns(address) {
         return pools[index].contractPolicy;
     }
 
-    function getPoolOwner(uint index) public constant returns(address) {
+    function getPoolOwner(uint index) public view returns(address) {
         return pools[index].owner;
     }
 
-    function getPoolMaxPayout(uint index) public constant returns(uint) {
+    function getPoolMaxPayout(uint index) public view returns(uint) {
         return pools[index].maxPayout;
     }
 
-    function getPoolMinStake(uint index) public constant returns(uint) {
+    function getPoolMinStake(uint index) public view returns(uint) {
         return pools[index].minStake;
     }
 
-    function getPoolDeposit(uint index) public constant returns(uint) {
+    function getPoolDeposit(uint index) public view returns(uint) {
         return pools[index].deposit;
     }
 
-    function getPoolBonusExpert(uint index) public constant returns(uint) {
+    function getPoolBonusExpert(uint index) public view returns(uint) {
         return pools[index].bonusExpert;
     }
 
-    function getPoolBonusFirstExpert(uint index) public constant returns(uint) {
+    function getPoolBonusFirstExpert(uint index) public view returns(uint) {
         return pools[index].bonusFirstExpert;
     }
 
-    function getPoolPayPeriod(uint index) public constant returns(uint) {
+    function getPoolPayPeriod(uint index) public view returns(uint) {
         return pools[index].payPeriod;
     }
 
-    function getPoolMinStakeTime(uint index) public constant returns(uint) {
-        return pools[index].minStakeTime;
+    function getPoolMinStakeTime(uint index) public view returns(uint) {
+        return pools[index].minStakeTimeInBlocks;
     }
 
-    function getPoolTimeout(uint index) public constant returns(uint) {
-        return pools[index].timeout;
+    function getPoolTimeout(uint index) public view returns(uint) {
+        return pools[index].timeoutInBlocks;
     }
 
-    function getPoolTimeOfInit(uint index) public constant returns(uint) {
-        return pools[index].timeOfInit;
+    function getPoolTimeOfInit(uint index) public view returns(uint) {
+        return pools[index].timeOfInitInBlocks;
     }
 
-    function getPoolUrlOfAuditReport(uint index) public constant returns(string) {
+    function getPoolUrlOfAuditReport(uint index) public view returns(string) {
         return pools[index].urlOfAuditReport;
     }
 
@@ -115,13 +106,8 @@ contract QSPb is Ownable {
         uint payPeriod, 
         uint minStakeTime, 
         uint timeout,
-        bool audit
+        string urlOfAuditReport
     ) public payable {
-        string memory urlOfAuditReport = "";
-        if (audit) { 
-            //TODO: QSP.requestAudit(candidateContract); 
-            urlOfAuditReport = "";
-        }
         Pool memory p = Pool(
             candidateContract, 
             contractPolicy, 
@@ -134,7 +120,7 @@ contract QSPb is Ownable {
             payPeriod, 
             minStakeTime, 
             timeout, 
-            now, 
+            block.number, 
             urlOfAuditReport);
         pools.push(p);
     }
--- test/QSPbTest.js
+++ test/QSPbTest.js
@@ -8,9 +8,6 @@ contract('QSPb', function(accounts){
     const contractPolicy = accounts[1];
     const poolOwner = accounts[2];
 
-    beforeEach(async function () {
-    });
-
     it("should never fail", async function() {
         assert.true;
     });
@@ -20,20 +17,19 @@ contract('QSPb', function(accounts){
         assert.equal(await qspb.balance.call(), 0);
         await qspb.createPool(candidateContract, contractPolicy, 10, 1, 3, 5, 15, 10000, 100, true, 
             {from: poolOwner, value: web3.toWei('3', 'ether')});
-	await web3.eth.getBlockNumber(console.log);
         assert.equal(await qspb.getPoolsLength.call(), 1);
-	assert.equal(await qspb.getPoolCandidateContract(0), candidateContract);
-	assert.equal(await qspb.getPoolContractPolicy(0), contractPolicy);
-	assert.equal(await qspb.getPoolOwner(0), poolOwner);
-	assert.equal(await qspb.getPoolMaxPayout(0), 10);
-	assert.equal(await qspb.getPoolMinStake(0), 1);
-	assert.equal(await qspb.getPoolDeposit(0), web3.toWei('3', 'ether'));
-	assert.equal(await qspb.getPoolBonusExpert(0), 3);
-	assert.equal(await qspb.getPoolBonusFirstExpert(0), 5);
-	assert.equal(await qspb.getPoolPayPeriod(0), 15);
-	assert.equal(await qspb.getPoolMinStakeTime(0), 10000);
-	assert.equal(await qspb.getPoolTimeout(0), 100);
-	//assert.equal(await qspb.getPoolTimeOfInit(0), blockTimestamp);
-	assert.equal(await qspb.getPoolUrlOfAuditReport(0), "");
+        assert.equal(await qspb.getPoolCandidateContract(0), candidateContract);
+        assert.equal(await qspb.getPoolContractPolicy(0), contractPolicy);
+        assert.equal(await qspb.getPoolOwner(0), poolOwner);
+        assert.equal(await qspb.getPoolMaxPayout(0), 10);
+        assert.equal(await qspb.getPoolMinStake(0), 1);
+        assert.equal(await qspb.getPoolDeposit(0), web3.toWei('3', 'ether'));
+        assert.equal(await qspb.getPoolBonusExpert(0), 3);
+        assert.equal(await qspb.getPoolBonusFirstExpert(0), 5);
+        assert.equal(await qspb.getPoolPayPeriod(0), 15);
+        assert.equal(await qspb.getPoolMinStakeTime(0), 10000);
+        assert.equal(await qspb.getPoolTimeout(0), 100);
+        assert.equal(await qspb.getPoolTimeOfInit(0), web3.eth.getBlockNumber(console.log));
+        assert.equal(await qspb.getPoolUrlOfAuditReport(0), "");
     }); 
 });
