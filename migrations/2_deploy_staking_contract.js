4f153bdc226901f0d308b28ef36335e60635a563
Jan Gorzny <j.gorzny@gmail.com>
2018-10-12 19:38:49 -0400

    SP-4 Change author, fix whitespace

--- migrations/2_deploy_staking_contract.js
+++ migrations/2_deploy_staking_contract.js
@@ -1,10 +1,10 @@
 const QSPb = artifacts.require('QSPb');
 const LinkedListLib = artifacts.require('LinkedListLib');
-const QSPbtcr = artifacts.require('Registry');
+const Registry = artifacts.require('Registry');
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(LinkedListLib)
     .then(() => deployer.link(LinkedListLib, QSPb))
-    .then(() => deployer.deploy(QSPbtcr))
-    .then(() => deployer.deploy(QSPb, QSPbtcr.address));
+    .then(() => deployer.deploy(Registry))
+    .then(() => deployer.deploy(QSPb, Registry.address));
 };
