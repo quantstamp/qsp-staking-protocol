2e46ff472d2f2fe0ab3fc994d7c6b6317c748059
Jan Gorzny <j.gorzny@gmail.com>
2018-10-13 15:19:48 -0400

    SP-4 Add registry assignment test

--- test/QSPbTest.js
+++ test/QSPbTest.js
@@ -1,12 +1,14 @@
 const QSPb = artifacts.require('QSPb');
+const Registry = artifacts.require('Registry');
 
 contract('QSPb', function(accounts){
-  
+
   beforeEach(async function () {
     qspb = await QSPb.deployed();
+    registry = await Registry.deployed();
   });
 
-  it("should never fail", async function() {
-    assert.true;
-  }); 
+  it("should have the right registry address", async function() {
+    assert.equal(await qspb.getStakingRegistry(), registry.address);
+  });
 });
