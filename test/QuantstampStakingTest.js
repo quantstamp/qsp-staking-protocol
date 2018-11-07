81db83009bfb97e0c8001faef621bf042a230cf6
banescusebi <sebi@quantstamp.com>
2018-11-07 19:21:52 +0100

    Remove double require()

--- test/QuantstampStakingTest.js
+++ test/QuantstampStakingTest.js
@@ -2,7 +2,6 @@ const QuantstampStaking = artifacts.require('QuantstampStaking');
 const QuantstampToken = artifacts.require('QuantstampToken');
 const QuantstampStakingRegistry = artifacts.require('test/Registry');
 const QuantstampParameterizer = artifacts.require('test/Parameterizer');
-const QuantstampParameterizer = artifacts.require('Parameterizer');
 const Voting = artifacts.require('plcr-revival/contracts/PLCRVoting.sol');
 const ZeroBalancePolicy = artifacts.require('ZeroBalancePolicy');
 const CandidateContract = artifacts.require('CandidateContract');
