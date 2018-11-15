1c5b53828d8d1d095f07f5586be56400e3c681b8
Jan Gorzny <j.gorzny@gmail.com>
2018-11-15 17:08:25 -0500

    SP-66 Add trivial backdoor policy contract

--- test/CandidateContractTest.js
+++ test/CandidateContractTest.js
@@ -1,26 +1,38 @@
 const Util = require("./util.js");
 const ZeroBalancePolicy = artifacts.require('ZeroBalancePolicy');
 const CandidateContract = artifacts.require('CandidateContract');
+const TrivialBackdoorPolicy = artifacts.require('TrivialBackdoorPolicy');
 
 contract('CandidateContract', function(accounts) {
   let candidateContract;
-  let policy;
+  let zeroBalancePolicy;
+  let trivialBackdoorPolicy;
 
   beforeEach(async function () {
     candidateContract = await CandidateContract.deployed();
-    policy = await ZeroBalancePolicy.deployed();
+    zeroBalancePolicy = await ZeroBalancePolicy.deployed();
+    trivialBackdoorPolicy = await TrivialBackdoorPolicy.deployed();
   });
 
-  it("should not violate policy", async function() {
-    assert.equal(await policy.isViolated(candidateContract.address), false);
+  it("should not initially violate the zero-balance policy", async function() {
+    assert.equal(await zeroBalancePolicy.isViolated(candidateContract.address), false);
   });
 
-  it("should violate policy", async function() {
+  it("should violate the zero-balance policy after the balance is withdrawn", async function() {
     await candidateContract.withdraw(await candidateContract.balance.call());
-    assert.equal(await policy.isViolated(candidateContract.address), true);
-  });  
+    assert.equal(await zeroBalancePolicy.isViolated(candidateContract.address), true);
+  });
 
   it("should throw an error", async function() {
-    Util.assertTxFail(policy.isViolated(accounts[0]));
+    Util.assertTxFail(zeroBalancePolicy.isViolated(accounts[0]));
+  });
+
+  it("should not initially violate the trivial-backdoor policy", async function() {
+    assert.equal(await trivialBackdoorPolicy.isViolated(candidateContract.address), false);
+  });
+
+  it("should violate the trivial-backdoor policy after that policy status is updated", async function() {
+    await trivialBackdoorPolicy.updateStatus(true);
+    assert.equal(await trivialBackdoorPolicy.isViolated(candidateContract.address), true);
   });
 });
