a15a45e59c805ddd94b5d0d713202d1d17482a2d
Jan Gorzny <j.gorzny@gmail.com>
2018-11-19 20:53:22 -0500

    SP-66 Add sample Democratic, Trusted Opinion, and Supply policies

---
+++ contracts/test/DemocraticViolationPolicy.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.4.24;
+
+import "../IPolicy.sol";
+
+// Adapted from https://solidity.readthedocs.io/en/develop/solidity-by-example.html (19 November 2018)
+
+contract DemocraticViolationPolicy is IPolicy {
+
+    event Violated(bool value);
+
+    // This declares a new complex type which will
+    // be used for variables later.
+    // It will represent a single voter.
+    struct Voter {
+        bool voted;  // if true, that person already voted
+        uint vote;   // index of the voted proposal
+    }
+
+    // This declares a state variable that
+    // stores a `Voter` struct for each possible address.
+    mapping(address => Voter) public voters;
+
+    uint256 minimumNumberOfVotesForViolation;
+    uint256 numOfVotesReceived;
+
+    // Never directly interact with the candidate, but noted for voter's reference
+    address candidateContract;
+
+    constructor(uint256 minVotes, address contractAddress) public {
+        minimumNumberOfVotesForViolation = minVotes;
+        candidateContract = contractAddress;
+    }
+
+    function vote(uint proposal) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        numOfVotesReceived += 1;
+    }
+
+    function isViolated(address contractAddress) external view returns(bool) {
+        require(contractAddress == candidateContract);
+        if (numOfVotesReceived >= minimumNumberOfVotesForViolation) {
+            emit Violated(true);
+            return true;
+        } else {
+            emit Violated(false);
+            return false;
+        }
+    }
+
+}
---
+++ test/CandidateTokenTest.js
@@ -0,0 +1,25 @@
+const CandidateToken = artifacts.require('CandidateToken');
+const TotalSupplyNotExceededPolicy = artifacts.require('TotalSupplyNotExceededPolicy');
+
+contract('CandidateToken', function(accounts) {
+  const owner = accounts[0];
+
+  let candidateToken;
+  let totalSupplyPolicy;
+
+  beforeEach(async function () {
+    candidateToken = await CandidateToken.deployed();
+    totalSupplyPolicy = await TotalSupplyNotExceededPolicy.deployed();
+  });
+
+  it("should not initially violate the at most 2*10**18 minted tokens policy", async function() {
+    assert.equal(await totalSupplyPolicy.isViolated(candidateToken.address), false);
+  });
+
+  it("should violate the at most 2*10**18 minted tokens policy when too many (1) additional tokens are minted", async function() {
+    await candidateToken.mintToken(owner, 1);
+
+    assert.equal(await totalSupplyPolicy.isViolated(candidateToken.address), true);
+  });
+
+});
