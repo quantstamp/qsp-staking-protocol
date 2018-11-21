72f8ff4bd2656a90bc76dc2ed5e74fd85dad3f18
alex-murashkin <alexmur90@gmail.com>
2018-11-21 13:49:19 -0500

    Test added asserting failure when the address is not compatible with the token contract

--- test/CandidateTokenTest.js
+++ test/CandidateTokenTest.js
@@ -1,6 +1,7 @@
 const CandidateToken = artifacts.require('CandidateToken');
 const TotalSupplyNotExceededPolicy = artifacts.require('TotalSupplyNotExceededPolicy');
 const OwnerNotChangedPolicy = artifacts.require('OwnerNotChangedPolicy');
+const Util = require('./util.js');
 
 contract('CandidateToken', function(accounts) {
   const owner = accounts[0];
@@ -28,6 +29,10 @@ contract('CandidateToken', function(accounts) {
 
   it("should not violate the OwnerNotChangedPolicy if the owner remains the same", async function() {
     assert.equal(await ownerNotChangedPolicy.isViolated(candidateToken.address), false);
+  });
+
+  it("should throw an exception on OwnerNotChangedPolicy if the address is not compatible with Candidate Token", async function() {
+    Util.assertTxFail(ownerNotChangedPolicy.isViolated(totalSupplyPolicy.address));
   });
 
   it("should violate the OwnerNotChangedPolicy if the owner has changed", async function() {
