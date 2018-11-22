186cab4b6e603e56e862817a8daa46844e50e38d
Alex Murashkin <1721409+alex-murashkin@users.noreply.github.com>
2018-11-22 16:06:51 -0500

    'Owner not changed' policy added (#39)
    * 'Owner not changed' policy added
    
    * Test added asserting failure when the address is not compatible with the token contract

--- test/SamplePolicyTest.js
+++ test/SamplePolicyTest.js
@@ -10,6 +10,7 @@ const DemocraticViolationPolicy = artifacts.require('DemocraticViolationPolicy')
 const TrustedOpinionPolicy = artifacts.require('TrustedOpinionPolicy');
 const CandidateToken = artifacts.require('CandidateToken');
 const TotalSupplyNotExceededPolicy = artifacts.require('TotalSupplyNotExceededPolicy');
+const OwnerNotChangedPolicy = artifacts.require('OwnerNotChangedPolicy');
 const Registry = artifacts.require('test/Registry');
 const TCRUtil = require('./tcrutils.js');
 
@@ -134,13 +135,16 @@ contract('CandidateContract', function(accounts) {
 
 contract('CandidateToken', function(accounts) {
   const owner = accounts[0];
+  const newOwner = accounts[1];
 
   let candidateToken;
   let totalSupplyPolicy;
+  let ownerNotChangedPolicy;
 
   beforeEach(async function () {
     candidateToken = await CandidateToken.deployed();
     totalSupplyPolicy = await TotalSupplyNotExceededPolicy.deployed();
+    ownerNotChangedPolicy = await OwnerNotChangedPolicy.new(owner);
   });
 
   it("should not matter when the total supply policy is checked with a non-token address", async function() {
@@ -157,4 +161,16 @@ contract('CandidateToken', function(accounts) {
     assert.equal(await totalSupplyPolicy.isViolated(candidateToken.address), true);
   });
 
+  it("should not violate the OwnerNotChangedPolicy if the owner remains the same", async function() {
+    assert.equal(await ownerNotChangedPolicy.isViolated(candidateToken.address), false);
+  });
+
+  it("should throw an exception on OwnerNotChangedPolicy if the address is not compatible with Candidate Token", async function() {
+    Util.assertTxFail(ownerNotChangedPolicy.isViolated(totalSupplyPolicy.address));
+  });
+
+  it("should violate the OwnerNotChangedPolicy if the owner has changed", async function() {
+    candidateToken.transferOwnership(newOwner);
+    assert.equal(await ownerNotChangedPolicy.isViolated(candidateToken.address), true);
+  });
 });
