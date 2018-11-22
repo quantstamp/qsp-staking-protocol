e7d1448d5872c89abf0a1b57b2d36f49cd8d6c3a
Jan Gorzny <j.gorzny@gmail.com>
2018-11-22 13:07:09 -0500

    SP-66 Move policies to own folder

---
+++ contracts/policies/TrivialBackdoorPolicy.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.4.24;
+
+import "../IPolicy.sol";
+
+/// @title TrivialBackdoorPolicy - the policy is violated if someone says it is.
+/// @author Jan Gorzny
+
+contract TrivialBackdoorPolicy is IPolicy {
+
+    // Whether or not the policy was violated
+    bool public contractViolated;
+
+    function updateStatus(bool status) public {
+      contractViolated = status;
+    }
+
+    function isViolated(address contractAddress) external view returns(bool) {
+      return contractViolated;
+    }
+}
