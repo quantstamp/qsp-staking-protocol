5dc445d65d46b995533e430a76ad5934b17bddd2
Jan Gorzny <j.gorzny@gmail.com>
2018-11-27 17:34:52 -0500

    SP-94 fix merge issues

--- contracts/policies/StateNotChangedPolicy.sol
+++ contracts/policies/StateNotChangedPolicy.sol
@@ -3,13 +3,8 @@ pragma solidity 0.4.24;
 import "../IPolicy.sol";
 import "../test/CandidateContract.sol";
 
-
-<<<<<<< HEAD
-/// @title StateNotChangedPolicy - the policy is violated if the state has changed
-=======
 /// @title StateNotChangedPolicy - the policy is violated if the state, as
 ///                                indicated by an `enum`, has changed
->>>>>>> 55c974a56da9b8635e3e1dc38bc3ca1248459cad
 
 contract StateNotChangedPolicy is IPolicy {
 
