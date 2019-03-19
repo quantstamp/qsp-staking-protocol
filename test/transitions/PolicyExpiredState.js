d8419542068d80b33d5b53e9df22a1220306e724
Martin Derka <martin.derka@seznam.cz>
2019-03-19 17:28:38 -0400

    SP-255 withdrawClaim function according to the FSM (#113)
    * Adjusted function withdrawClaim. Includes:
    
    - Changes to the smart contract according to the new style
    - Uncommenting all tests
    - Fixing tests where necessary
    
    * Reformatting
    
    * Rephrased test description
    
    * Rephrased test label

--- test/transitions/PolicyExpiredState.js
+++ test/transitions/PolicyExpiredState.js
@@ -311,9 +311,8 @@ contract('PolicyExpiredState.js: check transitions', function(accounts) {
         policy.updateStatus(false);
         // wait until minStakingTime passes twice
         await mineUntilMinStakingTime(poolId, pool.minStakeTimeInBlocks);
-        // todo (sebi): uncomment assert after the FSM is implemented
-        // await qspb.withdrawClaim(poolId, {from : stakeholder});
-        // await assertPoolState(poolId, PoolState.Cancelled);
+        await qspb.withdrawClaim(poolId, {from : stakeholder});
+        await assertPoolState(poolId, PoolState.Cancelled);
       }
     );
 
@@ -326,9 +325,8 @@ contract('PolicyExpiredState.js: check transitions', function(accounts) {
         policy.updateStatus(true);
         // wait until minStakingTime passes twice
         await mineUntilMinStakingTime(poolId, pool.minStakeTimeInBlocks);
-        // todo (sebi): uncomment assert after the FSM is implemented
-        // await qspb.withdrawClaim(poolId, {from : stakeholder});
-        // await assertPoolState(poolId, PoolState.Cancelled);
+        await qspb.withdrawClaim(poolId, {from : stakeholder});
+        await assertPoolState(poolId, PoolState.Cancelled);
       }
     );
 
