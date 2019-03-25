b61bdcff24c906b2a3c3854435c917d610f4c2b7
Martin Derka <martin@quantstamp.com>
2019-03-25 18:47:24 +0000

    Limiting test

--- test/transitions/NotViolatedUnderFundedState.js
+++ test/transitions/NotViolatedUnderFundedState.js
@@ -18,7 +18,7 @@ const PoolState = Object.freeze({
 });
 
 
-contract('NotViolatedUnderfundedState.js: check transitions', function(accounts) {
+contract.only('NotViolatedUnderfundedState.js: check transitions', function(accounts) {
 
   const owner = accounts[0];
   const staker = accounts [1];
