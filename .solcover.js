50273a079731b2654d5a3c0557acc1a62074acdb
Sebastian Banescu <banescusebi@gmail.com>
2019-04-11 15:39:42 +0200

    Added truffle test in solcover.js

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 7545,
-    testCommand: 'ethereum-bridge -H localhost:7545 -a 1 && mocha --timeout 5000',
+    testCommand: 'cp ../truffle-coverage.js ./truffle.js && truffle test && ethereum-bridge -H localhost:7545 -a 1',
     copyPackages: ['openzeppelin-solidity'],
     skipFiles: ['test/Registry.sol', 'test/Parameterizer.sol', 'test/QuantstampToken.sol', 'test/CandidateToken.sol', 'test/CandidateAndPolicyContract.sol']
 };
