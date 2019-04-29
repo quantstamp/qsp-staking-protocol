3447f5da34d3b8de676721851793d9842a8d53d9
Ubuntu <ubuntu@ip-172-31-44-224.ec2.internal>
2019-04-29 14:16:53 +0000

    Added outputs of simulation

--- simulation/run-all-inputs.sh
+++ simulation/run-all-inputs.sh
@@ -2,7 +2,7 @@
 
 PWD=$(pwd)
 
-for index in $( seq 1 $1);
+for index in $( seq $1 $2);
 do
     echo "At run $index"
     sed -i -E "s@(file:\/\/).*(\/simulation\/input\/run)[0-9]+(.txt)@\1$PWD\2$index\3@g" simulation/StakingSimulation.js
